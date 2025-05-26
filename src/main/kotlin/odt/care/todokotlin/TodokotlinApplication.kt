25bbdc7488b13b13159496213d71210235908a71
E3tow1C <thepphithak.w@kkumail.com>
2025-05-27 00:30:44 +0700

    load environment variables from .env file for database configuration

--- src/main/kotlin/odt/care/todokotlin/TodokotlinApplication.kt
+++ src/main/kotlin/odt/care/todokotlin/TodokotlinApplication.kt
@@ -1,5 +1,6 @@
 package odt.care.todokotlin
 
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,12 @@ import org.springframework.boot.runApplication
 class TodokotlinApplication
 
 fun main(args: Array<String>) {
+    // Set as system properties so Spring can use them
+    val dotenv = dotenv()
+
+    System.setProperty("DB_NAME", dotenv["DB_NAME"])
+    System.setProperty("DB_USER", dotenv["DB_USER"])
+    System.setProperty("DB_PASSWORD", dotenv["DB_PASSWORD"])
+
     runApplication<TodokotlinApplication>(*args)
 }
