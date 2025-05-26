96caf38187af0c96d3500b3e73a72bf267f5a68b
E3tow1C <thepphithak.w@kkumail.com>
2025-05-26 23:29:59 +0700

    init with spring boot 3.5.0

---
+++ src/main/kotlin/odt/care/todokotlin/TodokotlinApplication.kt
@@ -0,0 +1,11 @@
+package odt.care.todokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodokotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodokotlinApplication>(*args)
+}
---
+++ src/test/kotlin/odt/care/todokotlin/TodokotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package odt.care.todokotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodokotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
